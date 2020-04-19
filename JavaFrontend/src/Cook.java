c445254a6c988ccdb57a4943bd3352b60e1115bf
Adam Gray <ajgray2017@gmail.com>
2020-04-19 12:48:49 -0400

    stubs built for server and cook

--- JavaFrontend/src/Cook.java
+++ JavaFrontend/src/Cook.java
@@ -1,25 +1,49 @@
+import java.sql.*;
+import java.util.Scanner;
 
 public class Cook {
+	
+    private static int get_option() {
 
-	public Cook() {
-		while (true) {
-          int option = get_option();
-          switch (option) {
-              case 1:
-                  use_database(conn);
-                  break;
-              case 2:
-                  sql_inject(conn);
-                  break;
-              case 3:
-                  prepared_statement(conn);
-                  break;
-              case 4:
-                  call_stored_procedure(conn);
-                  break;
-              case 5:
-                  System.exit(0);
-                  break;
+        System.out.println("\t1. 1. See all current orders");
+        System.out.println("\t1. 2. Complete an order");
+        System.out.println("\t1. 3. Logout");
+        
+        Scanner s = new Scanner(System.in);
+        return s.nextInt();
+    }
+	
+	public static void main(Connection conn){
+		
+        while (true) {
+            int option = get_option();
+            switch (option) {
+                case 1:
+                    seeCurrentOrders(conn);
+                    break;
+                case 2:
+                    completeOrder(conn);
+                    break;
+                case 3:
+                    logout(conn);
+                    break;
+
+                }
+        }
+    }
+
+	private static void completeOrder(Connection conn) {
+		// TODO Auto-generated method stub
+		
 	}
 
+	private static void logout(Connection conn) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	private static void seeCurrentOrders(Connection conn) {
+		// TODO Auto-generated method stub
+		
+	}
 }
--- JavaFrontend/src/Server.java
+++ JavaFrontend/src/Server.java
@@ -1,8 +1,58 @@
+import java.sql.*;
+import java.util.Scanner;
 
 public class Server {
 
-	public Server() {
-		// TODO Auto-generated constructor stub
+	private static int get_option() {
+
+		System.out.println("\t1. 1. Place an order");
+		System.out.println("\t1. 2. See and edit your current orders");
+		System.out.println("\t1. 3. Get reciept");
+		System.out.println("\t1. 4. Logout");
+
+		Scanner s = new Scanner(System.in);
+		return s.nextInt();
 	}
 
+	public static void main(Connection conn) {
+
+		while (true) {
+			int option = get_option();
+			switch (option) {
+			case 1:
+				placeOrder(conn);
+				break;
+			case 2:
+				ServerView(conn);
+				break;
+			case 3:
+				getReciept(conn);
+				break;
+			case 4:
+				logout(conn);
+				break;
+
+			}
+		}
+	}
+
+	private static void placeOrder(Connection conn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void ServerView(Connection conn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void getReciept(Connection conn) {
+		// TODO Auto-generated method stub
+
+	}
+
+	private static void logout(Connection conn) {
+		// TODO Auto-generated method stub
+
+	}
 }
