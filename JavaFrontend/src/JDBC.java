bbaa41fd52d90c39bc07dbafec007ea83e60cc92
unknown <ajc8467@uncw.edu>
2020-04-19 12:54:35 -0400

    added a prepared statement to get the position of employee...

--- JavaFrontend/src/JDBC.java
+++ JavaFrontend/src/JDBC.java
@@ -5,7 +5,6 @@
  */
 
 import java.sql.*;
-import java.io.Console;
 import java.util.Scanner;
 
 public class JDBC
@@ -30,6 +29,33 @@ public class JDBC
         
         return conn;
     }
+    
+    public static String get_position(Connection conn) {
+    	String rslt = null;
+    	try {
+    	// do queries here that will be protected by a prepared statement
+    	Scanner s = new Scanner(System.in);
+    	System.out.print("Enter EID: ");
+    	String eid = s.nextLine();
+    	
+    	PreparedStatement pStmt = conn.prepareStatement(
+    			"select position from employee where eid = ?");
+    	
+    	pStmt.setString(1, eid);
+    	ResultSet rset = pStmt.executeQuery();
+    	
+    	while (rset.next()) {
+    		rslt = rset.getString(1);
+    	}
+    	
+    	} catch (SQLException e) {
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState:     " + e.getSQLState());
+            System.out.println("VendorError:  " + e.getErrorCode());
+        }
+    	
+    	return rslt;
+    }
   
     public static void main(String[] args) {
         String database_name, username,password;
@@ -39,23 +65,10 @@ public class JDBC
         password = "xjB6cq2I1";
 
         Connection conn = establish_connection(database_name, username, password);
-        Scanner s = new Scanner(System.in);
-        System.out.println("Enter in EID: ");
-        String eid = s.nextLine();
         
-        try {
-        	Statement stmt = conn.createStatement();
-        	ResultSet rset = stmt.executeQuery("select Position from employee where EID ='"+eid+"'");
-        	while (rset.next()) {
-                System.out.println(rset.getString(2));
-        	}
-        }
-        catch (SQLException e) {
-            System.out.println("SQLException: " + e.getMessage());
-            System.out.println("SQLState:     " + e.getSQLState());
-            System.out.println("VendorError:  " + e.getErrorCode());
-        }
+        String pos = get_position(conn);
         
+        System.out.println(pos);
     }
 }
 
