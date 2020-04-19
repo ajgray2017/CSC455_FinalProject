e4dfa23a7d68a5039adb1a4f4241a446a82d3fa3
Adam Gray <ajgray2017@gmail.com>
2020-04-19 12:25:00 -0400

    update

--- JavaFrontend/src/Cook.java
+++ JavaFrontend/src/Cook.java
@@ -2,7 +2,24 @@
 public class Cook {
 
 	public Cook() {
-		// TODO Auto-generated constructor stub
+		while (true) {
+          int option = get_option();
+          switch (option) {
+              case 1:
+                  use_database(conn);
+                  break;
+              case 2:
+                  sql_inject(conn);
+                  break;
+              case 3:
+                  prepared_statement(conn);
+                  break;
+              case 4:
+                  call_stored_procedure(conn);
+                  break;
+              case 5:
+                  System.exit(0);
+                  break;
 	}
 
 }
--- JavaFrontend/src/JDBC.java
+++ JavaFrontend/src/JDBC.java
@@ -212,27 +212,6 @@ public class JDBC
             System.out.println("VendorError:  " + e.getErrorCode());
         }
         
-        
-//        while (true) {
-//            int option = get_option();
-//            switch (option) {
-//                case 1:
-//                    use_database(conn);
-//                    break;
-//                case 2:
-//                    sql_inject(conn);
-//                    break;
-//                case 3:
-//                    prepared_statement(conn);
-//                    break;
-//                case 4:
-//                    call_stored_procedure(conn);
-//                    break;
-//                case 5:
-//                    System.exit(0);
-//                    break;
-//            }
-//        }
     }
 }
 
